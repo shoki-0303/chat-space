d67839b29a8609a7657c608822d1e20e254f9c8f
shoki-0303 <shoki522.33333@gmail.com>
2018-06-16 06:08:15 +0900

    create_automatic_update_function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function() {
   function append_message(message) {
     var image_html = (message.image_url) ? `<li class="image"><img src=${message.image_url} class="lower-message__image"></li>` : ""
-    var html =   `<ul class="message">
+    var html =   `<ul class="message" data-id="${message.id}">
                     <li class="content__middle__message__name">
                      ${ message.user_name }
                     </li>
@@ -38,5 +38,52 @@ $(function() {
       alert('error');
     })
   });
+
+  function updateHTML(new_message) {
+    var image_url = ""
+    if (new_message.image_url != null) {
+      var image_url = `<li class="image"><img src=${new_message.image_url} class="lower-message__image"></li>`
+    }
+    var html =   `<ul class="message" data-id="${new_message.id}">
+                    <li class="content__middle__message__name">
+                     ${ new_message.user_name }
+                    </li>
+                    <li class="content__middle__message__time">
+                     ${ new_message.created_at }
+                    </li>
+                    <li class="content__middle__message__text">
+                     ${ new_message.content }
+                    </li>
+                    ${ image_url }
+                  </ul>`
+    $('.content__middle__message').append(html);
+  }
+
+  $(function() {
+    setInterval(update, 5000);
+  });
+
+  function update() {
+    var message_id = $('.message:last').data('id');
+    if (location.href.match(/\/groups\/\d+\/messages/)) {
+      $.ajax({
+        url: location.href,
+        type: 'GET',
+        data: {id: message_id},
+        dataType: 'json'
+      })
+      .done(function(data){
+        console.log("done")
+        data.forEach(function(data) {
+          updateHTML(data);
+          console.log("done")
+        });
+      })
+      .fail(function(){
+        alert('自動更新に失敗しました');
+      })
+      $('.content__middle__message').animate({scrollTop: $('.content__middle__message')[0].scrollHeight}, 'slow');
+    }
+  }
 });
 
